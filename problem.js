09ecba2d563d597f24576eaf47ea2401f97f7978
uditgulati <uditgulati0@gmail.com>
2019-01-07 21:16:53 +0530

    Add remove button

--- problem.js
+++ problem.js
@@ -19,7 +19,28 @@ if(firstChar == firstChar.toUpperCase()) {
 	console.log(`Name ${prob_name}`);
 
 	var modName = prob_name.cloneNode(true);
-	modName.appendChild(htmlToNode(`<button onclick="localStorage.setItem('test', '0');
+
+	var arr = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
+	console.log(arr);
+	var set = new Set(arr);
+	console.log(set.size);
+
+	if(set.has(name)) {
+		modName.appendChild(htmlToNode(`<button onclick="localStorage.setItem('test', '0');
+			var arr = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
+			console.log(arr);
+			var set = new Set(arr);
+			console.log(set.size);
+			console.log('${name}');
+			set.delete('${name}');
+			var newArr = [...set];
+			localStorage.setItem('items', JSON.stringify(newArr));
+			location.reload();"
+		class="btn btn-primary" style="font-size: medium;margin-left: 10px;background-color: red;"> 
+		<i class="fa fa-minus" aria-hidden="true"></i> ToDo!</button>`));
+	}
+	else {
+		modName.appendChild(htmlToNode(`<button onclick="localStorage.setItem('test', '0');
 			var arr = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
 			console.log(arr);
 			var set = new Set(arr);
@@ -27,9 +48,11 @@ if(firstChar == firstChar.toUpperCase()) {
 			console.log('${name}');
 			set.add('${name}');
 			var newArr = [...set];
-			localStorage.setItem('items', JSON.stringify(newArr));"
+			localStorage.setItem('items', JSON.stringify(newArr));
+			location.reload();"
 		class="btn btn-primary" style="font-size: medium;margin-left: 10px"> 
 		<i class="fa fa-plus" aria-hidden="true"></i> ToDo!</button>`));
+	}
 
 	prob_name.parentNode.insertBefore(modName, prob_name);
 	prob_name.style.display = 'none';
